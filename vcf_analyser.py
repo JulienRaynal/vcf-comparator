abd25f230b80d11a5575eba909df6c43f950bf28
penpen <59729220+JulienRaynal@users.noreply.github.com>
2023-09-20 16:09:07 -0400

    updated the merge to also merge on the alt sequence

--- vcf_analyser.py
+++ vcf_analyser.py
@@ -26,24 +26,27 @@ def merge_formater(df: pd.DataFrame):
 
     # TODO: change the column passage from names like P15 to TRUE if name of FALSE if not 
 # TODO: logique pour la boucle
-    
-    merge_df = passage_df_list[0].copy()
-    merge_df = merge_df.rename(columns={"PASSAGE": merge_df["PASSAGE"].unique()[0]})
+    merge_df = passage_df_list[0].rename(columns={"PASSAGE": passage_df_list[0]["PASSAGE"].unique()[0]})
     for i in range(1, len(passage_df_list)):
         # Rename the PASSAGE column to the passage value
         passage_df_list[i] = passage_df_list[i].rename(columns={"PASSAGE": passage_df_list[i]["PASSAGE"].unique()[0]})
-        merge_df: pd.DataFrame = pd.merge(merge_df, passage_df_list[i], how="left", on=["ID", "END"])
-        print(merge_df.columns)
+        merge_df: pd.DataFrame = merge_df.copy().merge(passage_df_list[i], how="outer", on=["ID", "END", "ALT"])
         # delete all the duplicated _y columns
         merge_df = merge_df.loc[:, ~merge_df.columns.str.endswith("_y")]
         # remove _x from the columns
         merge_df.columns = merge_df.columns.str.rstrip("_x")
         # Move the passage column to the left
         passage_column_names: list = merge_df.filter(regex=("P[0-9]{2}")).columns.values.tolist()
-        print(passage_column_names)
         merge_df = merge_df[[c for c in merge_df if c not in passage_column_names] + passage_column_names]
-        print(merge_df.head)
 
+    # Fill the columns with boolean values
+    for passage in passages:
+        merge_df[passage] = merge_df[passage].replace({passage: True})
+        merge_df[passage] = merge_df[passage].fillna(False)
+
+
+    merge_df = merge_df.drop_duplicates()
+    return merge_df
     ## TODO: verifier si trim à 1000, sinon check si même mut dasn le on
     #merge_df: pd.DataFrame = pd.merge(passage_df_list[0], passage_df_list[1], how="left", on=["ID", "END"])
     ## Rename the left join passags to the passage names
@@ -61,4 +64,7 @@ def merge_formater(df: pd.DataFrame):
 
     
 file: pd.DataFrame = open_file("oscur.txt")
-merge_formater(file)
+#test = file[file["PASSAGE"] == "P30"].drop_duplicates()
+#print(test.shape)
+file2 = merge_formater(file)
+print(file2.loc[(file2["P30"] == False) & (file2["P15"] == True) & (file2["P50"] == True)].shape)
